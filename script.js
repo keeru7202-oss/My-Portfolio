7bedd41388ff94e1c99ca888671f7cf452b4f7c8
keeru7202-oss <keeru7202@gmail.com>
2026-09-02 19:40:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Welcome message
+
+window.addEventListener("load", function() {
+
+    console.log(
+        "Welcome to my portfolio!"
+    );
+
+});
+
+
+// Button interaction
+
+function showMessage() {
+
+    alert(
+        "Thank you for visiting my portfolio!"
+    );
+
+}
